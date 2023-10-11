1a7a0512b989166fd8f1e78e86ef5852171976c3
Avery Myers <averyjmyers@hotmail.com>
2023-10-10 19:17:05 -0700

    login query moved to mutation fixed the route in ligin

--- client/src/Pages/Login/index.jsx
+++ client/src/Pages/Login/index.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../../utils/queries';
+import { LOGIN_USER } from '../../utils/mutations';
 
 import Auth from '../../utils/auth'
 
