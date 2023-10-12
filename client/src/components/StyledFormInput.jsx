f824904374f16099948838200faaae727e15b712
Edward Von Schondorf <edvonschondorf@gmail.com>
2023-10-12 15:27:28 -0700

    Add Styled Form Input Component, replace all inputs in signup page with new component

---
+++ client/src/components/StyledFormInput.jsx
@@ -0,0 +1,40 @@
+// Form Input Components used in the project
+
+
+export default function StyledFormInput({
+  onChange,
+  type,
+  name,
+  placeholder,
+  required,
+  id,
+  minLength,
+  maxLength,
+  halfWidthStyle,
+  fullWidthStyle,
+}) {
+  
+  let inputStyle = "";
+
+  if (halfWidthStyle) {
+    inputStyle =
+      "mb-5 w-1/2 rounded-md border border-gray-300 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500";
+  } else if (fullWidthStyle) {
+    inputStyle =
+      "mb-5 w-full rounded-md border border-gray-300 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500";
+  }
+
+  return (
+    <input
+      className={inputStyle}
+      onChange={onChange}
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      required={required}
+      id={id}
+      minLength={minLength}
+      maxLength={maxLength}
+    />
+  );
+}
