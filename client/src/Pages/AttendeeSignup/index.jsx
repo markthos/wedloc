3f672e5949a1c20c290cefbaaa8576e6840feae0
Edward Von Schondorf <edvonschondorf@gmail.com>
2023-10-13 10:01:42 -0700

    Add basic styling to page

--- client/src/Pages/AttendeeSignup/index.jsx
+++ client/src/Pages/AttendeeSignup/index.jsx
@@ -1,9 +1,12 @@
 // Attendee Signup Page
+// TODO: Add save to database functionality
+// TODO: Make this look nicer, add a background image, icons, etc.
 
-//!TODO THIS NEEDS DATABASE SAVE
 
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import StyledButton from "../../components/StyledButton";
+import StyledFormInput from "../../components/StyledFormInput";
 
 export default function AttendeeSignup() {
   const navigate = useNavigate();
@@ -31,19 +34,26 @@ export default function AttendeeSignup() {
   };
 
   return (
-    <main className="min-h-screen bg-main_bg">
-      <section className="container m-auto">
-        <h1>What's Your name?</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="First Name"
-            value={attendee}
-            onChange={handleInputChange}
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </section>
-    </main>
+    <section className="container m-auto flex h-full items-center justify-center p-5">
+      <form
+        className="flex w-full flex-col items-center rounded-md bg-beige p-10 shadow-lg md:w-1/2"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="firstName" className="mb-5 text-xl">
+          What is your Name?
+        </label>
+        <StyledFormInput
+          fullWidthStyle
+          onChange={handleInputChange}
+          type={"text"}
+          name={"firstName"}
+          placeholder={"First Name"}
+          value={attendee}
+        />
+        <StyledButton primaryColor submit>
+          Submit
+        </StyledButton>
+      </form>
+    </section>
   );
 }
