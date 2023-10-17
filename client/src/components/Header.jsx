a90f66687bedc90c51eb922f2cba88ab8f6d9e54
Mark T <aydenthos@gmail.com>
2023-10-17 03:33:54 -0700

    working on profile pic, and updated navlink setup

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,76 @@
 // The site header where the navigation and logo/profile pic live
 
-
+import React, { useState, useEffect } from 'react';
 import NavMenu from "./NavMenu";
 import { Link as RouterLink } from 'react-router-dom';
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AuthService from '../utils/auth';
+import axios from 'axios'; // Import Axios
 
 export default function Header() {
+  const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    // Check if the user is authenticated
+    if (AuthService.loggedIn()) {
+      // Fetch user profile data using the JWT token
+      axios.get('/api/user/profile', {
+        headers: {
+          Authorization: `Bearer ${AuthService.getToken()}`,
+        },
+      })
+      .then((response) => {
+        setUserProfile(response.data)
+        console.log(setUserProfile);
+      })
+      .catch((error) => {
+        console.error('Error fetching user profile:', error);
+      });
+    }
+  }, []);
 
   return (
     <header className="relative bg-white">
       {/* Site Logo */}
       <span className="pt-3 text-center font-logo text-4xl md:text-6xl block">
-        <RouterLink to={"/"}>WedLoc</RouterLink>
+        <RouterLink to="/">WedLoc</RouterLink>
       </span>
-      {/* Profile Icon, if logged in it will be an image */}
+      {/* Profile Icon or Image */}
       <div className="absolute right-16 top-1/2 -translate-y-1/2 transform">
-        <RouterLink to={"/profile"}>
-          <AccountCircleIcon fontSize="large" />
-        </RouterLink>
+        {userProfile ? (
+          <RouterLink to="/profile">
+            <img src={userProfile.profilePic} alt="Profile" />
+          </RouterLink>
+        ) : (
+          <RouterLink to="/profile">
+            <AccountCircleIcon fontSize="large" />
+          </RouterLink>
+        )}
       </div>
       {/* Navigation Menu */}
       <NavMenu />
-
     </header>
   );
 }
+
+
+
+
+
+  // return (
+  //   <header className="relative bg-white">
+  //     {/* Site Logo */}
+  //     <span className="pt-3 text-center font-logo text-4xl md:text-6xl block">
+  //       <RouterLink to={"/"}>WedLoc</RouterLink>
+  //     </span>
+  //     {/* Profile Icon, if logged in it will be an image */}
+  //     <div className="absolute right-16 top-1/2 -translate-y-1/2 transform">
+  //       <RouterLink to={"/profile"}>
+  //         <AccountCircleIcon fontSize="large" />
+  //       </RouterLink>
+  //     </div>
+  //     {/* Navigation Menu */}
+  //     <NavMenu />
+
+  //   </header>
+  // );
